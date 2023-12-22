cd2559a60a912aa8a52d25ea55ac446edf3e7cbe
zc <zc-yun@outlook.com>
2023-12-22 23:08:17 +0800

    1

--- plugins/zp123_单选下拉框.js
+++ plugins/zp123_单选下拉框.js
@@ -2,13 +2,13 @@ import React from "react"
 import css from "../css/zp123_单选下拉框.css"
 
 function render(ref) {
-    const { props, input, labels } = ref
+    let { props, input, labels } = ref
     if (!props.dbf) return <div>请配置表单字段</div>
     const value = ref.getForm(props.dbf)
     const idx = ref.options.indexOf(value)
     return <React.Fragment>
         <div onClick={() => pop(ref)} className={(ref.open ? "open": "") + (props.insertRmIcon && labels[idx] ? " zhas": "") + (input ? " zfiltered": "")} >
-            {ref.open && props.filter && <input onChange={e => {input = e.target.value; ref.render()}} autoComplete="off"/>}
+            {ref.open && props.filter && <input onChange={e => {ref.input = e.target.value; ref.render()}} autoComplete="off"/>}
             <p>{labels[idx] || props.emptyLabel}</p>
             <svg className="zsvg zsvg_arrow" viewBox="64 64 896 896"><path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"/></svg>
             {!!props.insertRmIcon && <svg className="zsvg zsvg_x" onClick={e => {e.stopPropagation(); ref.setForm(ref.props.dbf)}} viewBox="64 64 896 896"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"/></svg>}
